67f8894fc3884d45d36bab033a706043a4b3add6
Karsten Knese <karsten.knese@googlemail.com>
2014-05-15 13:04:43 +0200

    grasping with gaze task in hand

--- demofiles/demo_ball_grasping.py
+++ demofiles/demo_ball_grasping.py
@@ -18,12 +18,16 @@ def followMarker():
 def speedUp(gainvalue):
     taskRW.task.controlGain.value = gainvalue
 
+def grasp():
+    pop(taskRW_safe)
+    push(taskGRASP)
+
 def resetRW():
     pop(taskGRASP)
     push(taskRW_safe)
 
 # Create basic tasks
-taskRW = createEqualityTask('rightWrist', 'arm_right_tool_joint')
+taskRW = createEqualityTask('rightWrist', 'hand_right_sot_grasping_frame_joint')
 
 taskRW_safe = createEqualityTask('rightWrist_safe', 'arm_right_tool_joint')
 quat = numpy.array([0.0,0.7071067811865476,0.0,0.7071067811865476])
@@ -35,8 +39,8 @@ gotoNd(taskRW_safe, rw_safe_goal, '111111',10)
 diag = (0,0,0,0,0,0,1.36499,1.49997,0.677897,0.636507,0.170576,0.234007,0.120986,0.0156722,0.0213592,0.687228,0.63189,0.172151,0.260947,0.120986,0.0156722,0.0213592,0.511255,0.520094)
 taskWEIGHTS = createWeightsTask(diag)
 
-taskGRASP = createGraspingTask('arm_right_tool_joint')
-taskGRASP.goto3D((0.4,0.0,1.3),10)
+taskGRASP = createGraspingTask('hand_right_sot_grasping_frame_joint')
+taskGRASP.goto3D((0.4,0.0,1.3),1)
 
 basicStack()
 gotoNd(taskRW,(0.4,0.0,1.3),'111',1)
